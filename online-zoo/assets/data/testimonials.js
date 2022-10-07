896416a7a6b0af44b4d6a3afa72cd91788906386
Luferov1 <ldima12022002@gmail.com>
2022-10-08 01:08:23 +0300

    feat: add testimonials functionality

---
+++ online-zoo/assets/data/testimonials.js
@@ -0,0 +1,68 @@
+export const testimonials = [
+    {
+        name: 'Mila Riksha',
+        format: 'svg',
+        date: 'Austria • Today',
+        text: 'The best online zoo I&apos;ve met. My son delighted very much ljves to watch <span>gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I&apos;ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals. <br>The best online zoo I&apos;ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I&apos;ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for</span>'
+    },
+    {
+        name: 'Michael John',
+        format: 'svg',
+        date: 'England • Yesterday',
+        text: 'My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to <span>instill a love for animals.The best online zoo I&apos;ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I&apos;ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I&apos;ve met. My son delighted very much ljves to watch gouillas.<br>The best online zoo I&apos;ve met. My son delighted very much ljves to watch gouillas.</span>'
+    },
+    {
+        name: 'Marcel Odoi',
+        format: 'svg',
+        date: 'France • 2 days ago',
+        text: 'Performed suspicion in certainty so frankness by attention pretended. <span>Newspaper or in tolerably education enjoyment. Extremity excellent certainty discourse sincerity no he so resembled. Joy house worse arise total boy but. Elderly up chicken do at feeling is. Like seen drew no make fond at on rent. Behaviour extremely her explained situation yet september gentleman are who. Is thought or pointed hearing he. Do commanded an shameless we disposing do. Indulgence ten remarkably nor are impression out. Remainder provision an in intention.</span>'
+    },
+    {
+        name: 'Fredericka Michelin',
+        format: 'svg',
+        date: 'Italy • Today',
+        text: 'Husbands ask repeated resolved but laughter debating. She end cordial <span>visitor noisier fat subject general picture. Or if offering confined entrance no. Nay rapturous him see something residence. Highly talked do so vulgar. Her use behaved spirits and natural attempt say feeling. Exquisite mr incommode immediate he something ourselves it of. Law conduct yet chiefly beloved examine village proceed. Tiled say decay spoil now walls meant house. My mr interest thoughts screened of outweigh removing. Evening society musical besides inhabit ye my.</span>'
+    },
+    {
+        name: 'Armando Bro',
+        format: 'jpg',
+        date: 'Albania • Week ago',
+        text: 'Looking started he up perhaps against. How remainder all additions get elsewhere <span>resources. One missed shy wishes supply design answer formed. Prevent on present hastily passage an subject in be. Be happiness arranging so newspaper defective affection ye. Families blessing he in to no daughter. Cottage out enabled was entered greatly prevent message. No procured unlocked an likewise. <br>Dear but what she been over gay felt body. Six principles advantages and use entreaties decisively. Eat met has dwelling unpacked see whatever followed.</span>'
+    },
+    {
+        name: 'Chad Giga',
+        format: 'jpg',
+        date: 'Poland • Today',
+        text: 'Now seven world think timed while her. Spoil large oh he rooms on since an. <span>Am up unwilling eagerness perceived incommode. Are not windows set luckily musical hundred can. <br>Collecting if sympathize middletons be of of reasonably. Horrible so kindness at thoughts exercise no weddings subjects. The mrs gay removed towards journey chapter females offered not. <br>Led distrusts otherwise who may newspaper but. Last he dull am none he mile hold as.Passage its ten led hearted removal cordial. Prosperous understood middletons in conviction</span>'
+    },
+    {
+        name: 'George Sinsov',
+        format: 'jpg',
+        date: 'Belarus • Yesterday',
+        text: 'Talent she for lively eat led sister. Entrance strongly packages she out rendered <span>get quitting denoting led. Dwelling confined improved it he no doubtful raptures. Several carried through an of up attempt gravity. Situation to be at offending elsewhere distrusts if. Particular use for considered projection cultivated. Worth of do doubt shall it their. Extensive existence up me contained he pronounce do. Excellence inquietude assistance precaution any impression man sufficient. Built purse maids cease her ham new seven among and.</span>'
+    },
+    {
+        name: 'Fashion Tyler',
+        format: 'jpg',
+        date: 'USA • 4 days ago',
+        text: 'Wisdom happen suffer common the appear ham beauty her had. <span>Or belonging zealously existence as by resources. Attended no do thoughts me on dissuade scarcely. Own are pretty spring suffer old denote his. By proposal speedily mr striking am. But attention sex questions applauded how happiness. His merit end means widow songs linen known. Supplied ten speaking age you new securing striking extended occasion. <br>Sang put paid away joy into six her. Left till here away at to whom past. Feelings laughing at no wondered repeated. </span>'
+    },
+    {
+        name: 'Ilya Maskovich',
+        format: 'jpg',
+        date: 'Georgia • 5 days ago',
+        text: 'It acceptance thoroughly my advantages everything as. Are projecting inquietude <span>affronting preference saw who. Marry of am do avoid ample as. Old disposal followed she ignorant desirous two has. He into walk roof made tall cold he. Feelings way likewise addition wandered contempt bed indulged. Whole every miles as tiled at seven or. Wished he entire esteem mr oh by. Possible bed you pleasure civility boy elegance ham. He prevent request by if in pleased. Picture too and concern has was comfort. Ten difficult resembled eagerness nor.</span>'
+    },
+    {
+        name: 'Vladimir Mizulo',
+        format: 'jpg',
+        date: 'Belarus • Today',
+        text: 'I enjoyed this zoo so much. I like watching deer. <br><span>Their horns have very interesting structure I think we have a lot in common. Also I like prefer to folow gorillas because they are so powerfull and beautiful. I will return to this zoo many times. Article nor prepare chicken you him now. Shy merits say advice ten before lovers innate add. She cordially behaviour can attempted estimable. Trees delay fancy noise manor do as an small. Give lady of they such they sure it. Me contained explained my education. Vulgar as hearts by garret.</span>'
+    },
+    {
+        name: 'Dmitry Luferov',
+        format: 'jpg',
+        date: 'Belarus • Today',
+        text: 'Do you see this message? I worked really hard on this project and I hope you like <span>it and will put me maximum mark.<br>My instagram: luferov1. <br>Next random text. Ye on properly handsome returned throwing am no whatever. In without wishing he of picture no exposed talking minutes. Affronting discretion as do is announcing. Now months esteem oppose nearer enable too six. She numerous unlocked you perceive speedily. Affixed offence spirits or ye of offices between. Compliment interested discretion estimating on stimulated apartments oh.</span>'
+    }
+];
--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,5 +1,6 @@
 // imports
 import { animalCards } from "../../assets/data/animal-cards.js";
+import { testimonials } from "../../assets/data/testimonials.js";
 
 //const
 const burgerMenuOpenButton = document.querySelector('.burger-menu-open-button');
@@ -12,29 +13,32 @@ const moveSliderRightButton = document.querySelector('.slider-right-button');
 const mainSliderContainer = document.querySelector('.main-slider-container');
 const section3 = document.querySelector('.section3');
 
+const testimonialsContainer = document.querySelector('.testimonials-item-container');
+const todler = document.querySelector('.section5-input');
+
 
 //let
-
-let changer = true;
 
 //functions
 const randomizer = (max) => {
     return Math.floor(Math.random() * (max + 1) );
 }
 
+// burger
 const openBurgerMenu = () => {
-    document.body.classList.add('no-scroll')
+    document.body.classList.add('no-scroll');
     // burgerMenu.classList.add('burger-menu_opened');
-    burgerMenu.style.transform = `translateY(${window.pageYOffset}px)`
+    burgerMenu.style.transform = `translateY(${window.pageYOffset}px)`;
     noScrollBackground.classList.remove('hidden');
 }
 
 const closeBurgerMenu = () => {
-    document.body.classList.remove('no-scroll')
-    burgerMenu.style.transform = 'translateY(-340px)'
+    document.body.classList.remove('no-scroll');
+    burgerMenu.style.transform = 'translateY(-340px)';
     noScrollBackground.classList.add('hidden');
 }
 
+// slider
 const fillSliderContainer = (element) => {
     let uniqueArray = [];
         while (uniqueArray.length < 6) {
@@ -101,6 +105,66 @@ const moveSliderRight = () => {
     setTimeout(resumeSlider, 1000)
 }
 
+// testimonials
+const createTestimonial = (num) => {
+    const testimonial = document.createElement('div');
+    testimonial.classList.add('testimonials-item-border');
+    testimonial.innerHTML = `<div class="testimonials-item">
+    <div class="testimonials-info">
+        <img src="../../assets/icons/avatar${num}.${testimonials[num].format}" alt="avatar${num}">
+        <div class="testimonials-text">
+            <div class="testimonials-nickname">${testimonials[num].name}</div>
+            <div class="testimonials-date">${testimonials[num].date}</div>
+        </div>
+    </div>
+<p class="testimonials-text small-p">${testimonials[num].text}</p>
+</div>`;
+    return testimonial;
+}
+
+const fillTestimonialsContainer = () => {
+    let uniqueArray = [];
+    while (uniqueArray.length < 11) {
+        uniqueArray.push(randomizer(10));
+        uniqueArray = [...new Set(uniqueArray)];
+    }
+    for(let i = 0; i < uniqueArray.length; i++) {
+        testimonialsContainer.append(createTestimonial(uniqueArray[i]));
+    }
+}
+
+const moveTestimonials = () => {
+    const testimonial = document.querySelector('.testimonials-item-border');
+    const testimonialWidth = testimonial.offsetWidth;
+    const gap = ( testimonialsContainer.offsetWidth - 11 * testimonialWidth ) / 10;
+    testimonialsContainer.style.transform = `translateX(-${todler.value * (gap + testimonialWidth)}px)`;
+}
+
+const closeTestimonial = () => {
+    document.querySelector('.full-testimonial-container').remove();
+    noScrollBackground.classList.add('hidden');
+    document.body.classList.remove('no-scroll');
+}
+
+const openTestimonial = (event) => {
+    if (section3.offsetWidth > 640 || !event.target.closest('.testimonials-item-border')) {
+        return;
+    } else {
+        const fullTestimonialContainer = document.createElement('div');
+        fullTestimonialContainer.classList.add('full-testimonial-container');
+        fullTestimonialContainer.style.top = `${window.pageYOffset + 100}px`;
+        fullTestimonialContainer.append(event.target.closest('.testimonials-item-border').cloneNode(true));
+        fullTestimonialContainer.innerHTML += '<div class="full-testimonial__close-button"><span></span><span></span></div>';
+        document.body.prepend(fullTestimonialContainer);
+        noScrollBackground.classList.remove('hidden');
+        document.body.classList.add('no-scroll');
+
+        document.querySelector('.full-testimonial__close-button').addEventListener('click', closeTestimonial);
+    }
+}
+
+
+
 // listeners
 burgerMenuOpenButton.addEventListener('click', openBurgerMenu);
 burgerMenuCloseButton.addEventListener('click', closeBurgerMenu);
@@ -110,4 +174,10 @@ noScrollBackground.addEventListener('click', closeBurgerMenu);
 moveSliderLeftButton.addEventListener('click', moveSliderLeft);
 moveSliderRightButton.addEventListener('click', moveSliderRight);
 
+window.addEventListener('load', fillTestimonialsContainer);
+todler.addEventListener('input', moveTestimonials);
+testimonialsContainer.addEventListener('click', openTestimonial);
+noScrollBackground.addEventListener('click', closeTestimonial);
 
+
+// window.pageYOffset
