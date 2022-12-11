8cac64cc506a617e2aecaa2f0d14a2b08fb7c9a5
Luferov1 <ldima12022002@gmail.com>
2022-12-11 19:25:57 +0300

    feat: migrate controller/appLoader.js to ts

--- news-JS-main/src/components/controller/appLoader.ts
+++ news-JS-main/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'd07b05a8ef994f9fa8db55b92d548e63', // получите свой ключ https://newsapi.org/
         });
     }
 }
