bd70beb4e5abfbc79f4fda082e58c4364f738c8d
Luferov1 <ldima12022002@gmail.com>
2022-12-12 23:30:33 +0300

    feat: add adaptive

--- news-JS-main/src/index.ts
+++ news-JS-main/src/index.ts
@@ -1,5 +1,21 @@
 import App from './components/app/app';
 import './global.css';
 
+const sourceContainer = document.querySelector('.sources');
+const chooseAnotherSourceButton = document.querySelector('.choose-another-button');
+sourceContainer?.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target) {
+        if ((target as Element).classList.contains('source__item')) {
+            sourceContainer.classList.add('sources_hidden');
+            chooseAnotherSourceButton?.classList.add('choose-another-button_active');
+        }
+    }
+});
+chooseAnotherSourceButton?.addEventListener('click', () => {
+    sourceContainer?.classList.remove('sources_hidden');
+    chooseAnotherSourceButton?.classList.remove('choose-another-button_active');
+});
+
 const app = new App();
 app.start();
