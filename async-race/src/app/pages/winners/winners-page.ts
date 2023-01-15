fe7dbb0dc5f0488241b6302dad87a59655675580
Luferov1 <ldima12022002@gmail.com>
2023-01-15 16:27:02 +0300

    refactor: delete extra imports

--- async-race/src/app/pages/winners/winners-page.ts
+++ async-race/src/app/pages/winners/winners-page.ts
@@ -1,7 +1,5 @@
 import TagNames from '@/app/abstract/enums/tag-names-enum';
 import elementCreator from '@/app/abstract/functions/element-creator-func';
-import ButtonClassNames from '../../abstract/enums/button-classNames-enum';
-import ButtonText from '../../abstract/enums/button-text-enum';
 import createHeaders from '@/app/abstract/functions/create-headers-func';
 import createPaginationButtons from '@/app/abstract/functions/create-pagination-buttons';
 import './winners-page.scss';
