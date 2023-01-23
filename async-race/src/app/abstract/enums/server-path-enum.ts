f41741be6845775861bfae37415d425fb31dff8e
Luferov1 <ldima12022002@gmail.com>
2023-01-23 20:28:35 +0300

    refactor: change port

--- async-race/src/app/abstract/enums/server-path-enum.ts
+++ async-race/src/app/abstract/enums/server-path-enum.ts
@@ -1,7 +1,5 @@
-//TODO: change port in adress to 3000
-
 enum ServerPath {
-  address = 'http://127.0.0.1:8080',
+  address = 'http://127.0.0.1:3000',
   garage = '/garage',
   winners = '/winners',
   engine = '/engine',
